da30cdd6fbe31fadf04412e085a24b4de6a61658
Neha89007 <59912257+Neha89007@users.noreply.github.com>
2022-04-14 15:36:41 +0530

    Add files via upload

---
+++ LabPractical/EmployeeSalary.java
@@ -0,0 +1,13 @@
+class EmployeeSalary
+{
+    public static void main (String[] args) 
+    {
+        int E1 = 11000 ;
+        int E2 = 5000 ;
+        int E3 ;
+        
+        E3 = (10000 * 3)-(E1 + E2);
+        
+        System.out.println("Salary of third empolyee is " +E3);
+    }
+}
