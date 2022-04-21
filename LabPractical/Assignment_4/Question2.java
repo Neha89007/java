ad1145dfdb0495777bf01d89d176be353e4e75cb
Neha89007 <59912257+Neha89007@users.noreply.github.com>
2022-04-21 21:51:52 +0530

    Update Question2.java

--- LabPractical/Assignment_4/Question2.java
+++ LabPractical/Assignment_4/Question2.java
@@ -10,15 +10,15 @@ class Main
 	   System.out.println("Enter the population after a decade");
 	   int y  = sc.nextInt();
 	   
-	   int pop = y - x;
+	   double pop = y - x;
 	   System.out.println("population increased in 10 years ="+ pop);
 	   System.out.println(" ");
 	   
-	   int IncPer =( pop / x ) * 100;
+	   double IncPer =( pop / x ) * 100;
 	   System.out.println("increased percentage in 10 years =" + IncPer+ "%");
 	   System.out.println(" ");
 	   
-	   int per = IncPer/10;
+	   double per = IncPer/10;
 	   System.out.println("increased percentage for 1 years =" + per+ "%");
 	   System.out.println(" ");
 	}
